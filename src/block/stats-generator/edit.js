ec244172d7951782728f26012ddc07b5356fe444
R A Van Epps <rave@ravanepps.com>
2020-07-27 16:25:13 -0600

    Set up innerblocks props

--- src/block/stats-generator/edit.js
+++ src/block/stats-generator/edit.js
@@ -2,11 +2,38 @@
  * EDIT: Stats Generator Block
  */
 
+import statDice from './statDice';
+
 const {
 	blockEditor: {
 		InnerBlocks,
 	},
 } = wp;
+
+/**
+ * Set up InnerBlocks template prop.
+ *
+ * @author R A Van Epps <rave@ravanepps.com>
+ * @since  1.0.0
+ *
+ * @return {Array} Template prop config array.
+ */
+const getInnerBlocksTemplate = () => {
+	const template = [];
+
+	for ( let i = 0; 6 > i; i++ ) {
+		template.push( [ ...statDice ] );
+	}
+
+	return template;
+};
+
+// Set up InnerBlocks props.
+const innerBlocksProps = {
+	allowedBlocks: [ 'rave/dice-roller' ],
+	templateLock: 'all',
+	template: getInnerBlocksTemplate(),
+};
 
 /**
  * Handle edit functionality in the admin.
